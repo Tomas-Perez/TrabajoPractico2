c98d524200fdff0f5dc71b16b9c0b361592d66d0
Lethanity <manupedr@gmail.com>
2017-03-02 20:41:28 -0300

    Complex Number

--- test/ComplexNumberTest.java
+++ test/ComplexNumberTest.java
@@ -6,40 +6,40 @@ public class ComplexNumberTest {
     @Test
     public void getReal() throws Exception {
         ComplexNumber complex = new ComplexNumber(3,4);
-        assertEquals(3,complex.getReal());
+        assertEquals(3,complex.getReal(),0.1);
     }
 
     @Test
     public void getImaginary() throws Exception {
         ComplexNumber complex = new ComplexNumber(3,4);
-        assertEquals(4,complex.getImaginary());
+        assertEquals(4,complex.getImaginary(),0.1);
     }
 
     @Test
     public void sum() throws Exception {
         ComplexNumber complex1 = new ComplexNumber(3,4);
         ComplexNumber complex2 = new ComplexNumber(4,5);
-        assertEquals(new ComplexNumber(7,9),complex1.sum(complex2));
+        assertTrue(complex1.sum(complex2).equals(new ComplexNumber(7,9)));
     }
 
     @Test
     public void product() throws Exception {
         ComplexNumber complex1 = new ComplexNumber(3,4);
         ComplexNumber complex2 = new ComplexNumber(4,5);
-        assertEquals(new ComplexNumber(7,9),complex1.sum(complex2));
+        assertTrue(complex1.sum(complex2).equals(new ComplexNumber(7,9)));
     }
 
     @Test
     public void subtract() throws Exception {
         ComplexNumber complex1 = new ComplexNumber(3,4);
-        ComplexNumber complex2 = new ComplexNumber(3,4);
-        assertEquals(new ComplexNumber(-8,31),complex1.sum(complex2));
+        ComplexNumber complex2 = new ComplexNumber(4,5);
+        assertTrue(complex1.subtract(complex2).equals(new ComplexNumber(-1,-1)));
     }
 
     @Test
     public void absoluteValue() throws Exception {
         ComplexNumber complex = new ComplexNumber(-3,-4);
-        assertEquals(new ComplexNumber(3,4),complex);
+        assertTrue(complex.absoluteValue().equals(new ComplexNumber(3,4)));
     }
 
 }
